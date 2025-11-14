9251d70dd18157eeec67f00277e19cf66c1c4c65
Quistio <cristian.michel.borges@gmail.com>
2025-11-14 17:38:28 -0300

    Separação do projeto em dois repositorio, um de api e outro do front

--- api/src/config/sequelize.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-const configDataBase = require('./database');
-
-const sequelize = new Sequelize(configDataBase);
-
-module.exports = sequelize;
-
-
--- api/src/routes/cep.route.js
+++
@@ -1,9 +0,0 @@
-
-const router = require('express-promise-router')();
-const controller = require('../controllers/cep.controller');
-
-const verifyToken = require('../middleware/authMiddleware');
-
-router.get('/cep/:codCep', verifyToken, controller.listAll);
-
-module.exports = router;
